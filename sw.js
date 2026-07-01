b7c5aa0334c4a0ef54294055a98a074e45355f9e
ys3512872242-sudo <ys3512872242@gmail.com>
2026-07-01 23:17:57 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "companion-shell-v1";
+const CACHE_NAME = "companion-shell-v2";
 const SHELL_FILES = ["./index.html", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
